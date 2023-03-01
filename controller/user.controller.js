ad8cc5caf66d9e9a5c70a4e557b89a378f0030db
JspreetSingh <js9029@srmist.edu.in>
2023-03-01 14:14:30 +0530

    initial

---
+++ controller/user.controller.js
@@ -0,0 +1,15 @@
+const connection= require("../db/config");
+
+module.exports={
+    getAllUsers: (req,res)=>
+    {
+        connection.query("Select * from users",(err,results)=>{
+            if(err){
+                res.status(500).send("Error reteriving users");
+            }
+            else{
+                res.join
+            }
+        })
+    }
+}
---
+++ express/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const useRoutes=require("../routes/useRoutes")
+app.use(express.json());
+const port = 3002;
+
+app.use("/users",useRoutes);
+
+app.get("/", (req, res) => {
+    res.send({ hello: "world from get api endpoint" });
+});
+// app.post("/", (req, res) => {
+//     let a = parseInt(req.query.a);
+//     let b = parseInt(req.query.b);
+//     let name = req.query.name;
+//     let c = a + b;
+//     res.send({ result: c, user: name });
+// });
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+
+// in terminal:
+// npm i nodemon -g
+// nodemon index.js
